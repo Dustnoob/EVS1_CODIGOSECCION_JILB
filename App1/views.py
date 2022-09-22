ef794ee78cba89508c10a2798ae931fa11df7316
Javier Loncon <javier.loncon@inacapmail.cl>
2022-09-22 10:43:58 -0300

    Commit de APP1 confirmado1

--- App1/views.py
+++ App1/views.py
@@ -8,6 +8,17 @@ import datetime
 def display(request):
     return HttpResponse(
         '<h1>Hola Esta es mi APP1</h1>'
+        '<div><ul><li>1uno</li><ul>'
+        '<ul><li>2dos</li></ul>'
+        '<ul><li>3tres</li></ul>'
+        '<ul><li>4cuatro</li></ul>'
+        '<ul><li>5cinco</li></ul>'
+        '<ul><li>6seis</li></ul>'
+        '<ul><li>7siete</li></ul>'
+        '<ul><li>8ocho</li></ul>'
+        '<ul><li>9nueve</li></ul>'
+        '<ul><li>10diez</li></ul></div>'
+
     )
 
 def displayDateTime(request):
